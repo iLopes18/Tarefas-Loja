67bb4a00109cffb8206cbffc078f292b3d687dfc
iLopes18 <18isaaclopes@gmail.com>
2025-11-30 12:28:27 +0000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class', // <--- ESTA LINHA É OBRIGATÓRIA PARA O BOTÃO FUNCIONAR
   theme: {
     extend: {},
   },
